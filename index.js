312c7e948db539be37236f8df73356d9dcfa6f15
phowells <paul.howells@capgemini.com>
2016-04-25 12:40:42 +0100

    Added options to exclude Double Quote Marks and Single Quote Marks from the escaping replacements

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ module.exports = function (string, options) {
     // Escape all characters not included in SingleStringCharacters and
     // DoubleStringCharacters on
     // http://www.ecma-international.org/ecma-262/5.1/#sec-7.8.4
+    // unless the options argument request excludes the character from escaping.
+    if (options) {
+      switch (character) {
+        case '"':
+          if (options.excludeDoubleQuote) {
+            // Do not escape a Double Quote Mark.
+            return character;
+          }
+        case "'":
+          if (options.excludeSingleQuote) {
+            // Do not escape a Single Quote Mark.
+            return character;
+          }
+      }
+    }
     switch (character) {
       case '"':
       case "'":
